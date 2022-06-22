56f3d07964ab73da846b512cac9daa07bdf790de
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-06-22 16:32:43 +1200

    experiment using external api

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Messages from './Messages'
 import SendNewMessage from './SendNewMessage'
+import request from 'superagent'
 
 const initialState = [{ message: '' }]
 
 function App() {
   const [messages, setMessages] = useState(initialState)
-
+  const [image, setImage] = useState('')
   function handleAddMessage(newMessage) {
     setMessages([...messages, newMessage])
     console.log(messages)
   }
+  // experiment using fox api
+  useEffect(() => {
+    request.get('https://randomfox.ca/floof/').then(response => {
+      setImage(`"${response.body.image}"`)
+    })
+  }, [])
   
   return (
     <>
@@ -33,10 +40,11 @@ function App() {
         <main>
           <Messages messages={messages} />
           <SendNewMessage onAddMessage={handleAddMessage} />
+          <image src={image} alt="a fox"></image>
+          {console.log(image)}
         </main>
       </div>
     </>
-
   )
 }
 

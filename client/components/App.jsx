70b57bdd92bbac704bf237ebf0cc56f1d179cf27
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-07-02 10:47:10 +1200

    setup home menu, create choice redux state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
+import Home from './Home'
 import Circles from './Circles'
 import Conversation from './Conversation'
 import SendNewMessage from './SendNewMessage'
 
 function App() {  
+      /* const dice = useSelector((state) => state.dice)
   return (
-    <>
-      <div className="area" >
-        <Circles />
-      </div >
-      <div className="context">
-        <main>
-          <Conversation />
-          <SendNewMessage />
-        </main>
-      </div>
-    </>
+    <div className="container">{dice.rolled ? <Results /> : <Initial />}</div>
+  ) */
+
+  const choice = useSelector(state => state.menu.choice)
+    console.log(choice)
+  return (
+      <>
+        {choice ? <><Circles/><Conversation /><SendNewMessage /></>: <Home />}
+      </>
   )
 }
 

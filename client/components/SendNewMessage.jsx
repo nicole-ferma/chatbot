c81d5b0d296087eebb3c6d2de2f46730d2afd475
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-07-07 18:31:22 +1200

    wip media queries

--- client/components/SendNewMessage.jsx
+++ client/components/SendNewMessage.jsx
@@ -22,42 +22,6 @@ function SendNewMessage(props) {
     setNewMessage( evt.target.value)
   }
 
-  // useEffect(() => {
-  //   getResponse(newMessage)
-  //     .then(responseArray => {
-  //       setNewResponse(responseArray)
-  //       console.log(response)
-  //     })
-  //     .catch(err => {
-  //       console.log(err)
-  //     })
-  // }, [])
-
-  // function respondToMessage(newMessage) {
-  //   const greeting = /h[ea]llo|hi|howdy/i
-  //   const farewell = /bye|see.you|goodbye/i
-  //   const randomIndex = Math.floor(Math.random() * 3)
-  //   if(greeting.test(newMessage)) {
-  //     response && dispatch(addResponse(JSON.parse(response[0].responseArray)[randomIndex]))
-  //   } else if (farewell.test(newMessage)) {
-  //     response && dispatch(addResponse(JSON.parse(response[1].responseArray)[randomIndex]))
-  //   }
-  //   return 
-  // }
-
-  // this currently gets ALL responses from db -- refactor!!!
-  // useEffect(() => {
-  //   getResponses()
-  //     .then(response => {
-  //       setNewResponse(response)
-  //     })
-  //     .catch(err => {
-  //       console.log(err)
-  //     })
-  // }, [])
-
-
-
   return (
     <>
       <form onSubmit={handleSubmit}>
